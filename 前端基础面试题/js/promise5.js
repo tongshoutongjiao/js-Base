9cdbe18b93ed902b514bd12fc12e6ce2048e8207
202369 <1741969637@qq.com>
2021-12-13 17:48:06 +0800

    1213 git 代码提交

--- 前端基础面试题/js/promise5.js
+++ 前端基础面试题/js/promise5.js
@@ -5,47 +5,70 @@
  */
 
 let  p=new Promise(function (res,rej){
-
     setTimeout(function (){
         res('42')
     },100)
+})
 
+p.then(null,(err)=>{
+    console.log('错误')
+}).then(res=>{
+    console.log('继续将决议结果传递到下层')
+   return Promise.resolve({code:200,msg:'成功'})
+}).then(res=>{
+    console.log('返回最新的决议值');
+    console.log(res)
+})
+
+p.catch((res)=>{
+    console.log(res)
 })
 
 let p2=new Promise(function (res,rej){
     setTimeout(()=>{
         res('p2 函数结果决议')
-    },0)
+    },10)
 })
-console.log('查看结果 promise5');
-console.log(p)
-p.then(function (res){
-    console.log('查看执行结果');
-    console.log(res);
-    return p2
-}).then(res=>{
-    console.log('第二部问题');
-    console.log(res);
+// console.log('查看结果 promise5');
+// console.log(p)
+// p.then(function (res){
+//     console.log('查看执行结果');
+//     console.log(res);
+//     return p2
+// }).then(res=>{
+//     console.log('第二部问题');
+//     console.log(res);
+//
+// },err=>{
+//     console.log('错误捕获函数');
+//     console.log(err);
+//     return 42
+// })
+//
+// let p3=new Promise(function (res,rej){
+//
+//     setTimeout(()=>{
+//         console.log('查看结果');
+//         res('p2')
+//     },0)
+//
+// })
+// p3.then(res=>{
+//     console.log('查看结果');
+//     console.log('p2')
+//
+// },err=>{
+//     console.log('err');
+//     console.log(err)
+// })
 
-},err=>{
-    console.log('错误捕获函数');
-    console.log(err);
-    return 42
+
+Promise.all([p,p2]).then(res=>{
+    console.log('查看all');
+    console.log(res)
 })
 
-let p3=new Promise(function (res,rej){
-
-    setTimeout(()=>{
-        console.log('查看结果');
-        res('p2')
-    },0)
-
-})
-p3.then(res=>{
-    console.log('查看结果');
-    console.log('p2')
-
-},err=>{
-    console.log('err');
-    console.log(err)
+Promise.race([p,p2]).then(res=>{
+    console.log('race 决议结果');
+    console.log(res)
 })
